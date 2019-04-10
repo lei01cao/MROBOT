feb489453a1ee800c7e12412c3a77467ff274477
lei <lei.cao@hurys.com>
2019-04-10 19:42:38 +0800

    vfh

--- Algorithm/PathFollow.m
+++ Algorithm/PathFollow.m
@@ -55,6 +55,15 @@ rangeFinderModel.SensorPose = ...
 %laserSub = rossubscriber('/scan');
 odomSub = rossubscriber('/odom');
 
+%ObstacleAvoidance
+vfh = robotics.VectorFieldHistogram;
+vfh.DistanceLimits = [0.05 1];
+vfh.RobotRadius = 0.1;
+vfh.MinTurningRadius = 0.2;
+vfh.SafetyDistance = 0.1;
+
+targetDir = 0; %目标方向设置为0
+
 %初始化AMCL对象
 amcl = robotics.MonteCarloLocalization;
 amcl.UseLidarScan = true;
@@ -100,6 +109,28 @@ controlRate = robotics.Rate(20);
 while(distanceToGoal > goalRadius)   
 	[v, omega] = controller(robotCurrentPose);
     disp([v,omega]);
+
+    % Get laser scan data
+    laserScan = receive(laserSub);
+    ranges = double(laserScan.Ranges);
+    angles = double(laserScan.readScanAngles)   
+
+    % Call VFH object to computer steering direction
+    steerDir = vfh(ranges, angles, targetDir);    
+        % Calculate velocities
+    if ~isnan(steerDir) % If steering direction is valid
+        desiredV = 0.2;
+        w = ComputeAngularVelocity(steerDir, 1);
+    else % Stop and search for valid direction
+        desiredV = 0.0;
+        w = 0.5;
+    end
+
+    % Assign and send velocity commands
+    velMsg.Linear.X = desiredV;
+    velMsg.Angular.Z = w;
+    velPub.send(velMsg);
+
 	%drive(robot, v, omega);
     velMsg.Linear.X = v;
     velMsg.Angular.Z = omega;
